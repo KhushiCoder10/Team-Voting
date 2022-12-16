71f4b35d605c982b69ab22f3743bc6d2225e8aff
KhushiCoder10 <100891484+KhushiCoder10@users.noreply.github.com>
2022-12-15 19:52:52 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-
+  apiKey: "AIzaSyCsp6B-ozScVOfXIncqrdb4F-Ivz9GwJ2A",
+  authDomain: "assignment-67.firebaseapp.com",
+  projectId: "assignment-67",
+  storageBucket: "assignment-67.appspot.com",
+  messagingSenderId: "974730466075",
+  appId: "1:974730466075:web:4638cc5793e9cc9bcf79be",
+  measurementId: "G-YWTE1J5NGK"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
 
 export default firebase.database();
